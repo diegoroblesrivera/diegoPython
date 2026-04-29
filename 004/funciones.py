b4016b7c639e2b277c8c262e3066d08db337a232
Diegorr <diegoroblesrivera@gmail.com>
2026-04-29 12:37:05 -0400

    Ejemplo de uso de git en vSC

--- 004/funciones.py
+++ 004/funciones.py
@@ -15,5 +15,9 @@ def suma():
     n1=int(input("ingrese un numero: "))
     n2=int(input("ingrese otro numero: "))
     print(f"El resultado es {n1+n2}")
+def resta():
+    n1=int(input("ingrese un numero: "))
+    n2=int(input("ingrese otro numero: "))
+    print(f"El resultado es {n1-n2}")
 
 suma()
