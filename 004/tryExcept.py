133adfff93e560220db695144c5f4574f3404d37
Diegorr <diegoroblesrivera@gmail.com>
2026-05-12 16:37:53 -0400

    Actividadd 2.4.3

---
+++ 004/tryExcept.py
@@ -0,0 +1,69 @@
+# while True:
+#     try:
+#         num=int(input("Ingrese un numero: "))
+#         break
+#     except:
+#         print("Solo numeros enteros")
+    
+
+
+
+
+# while True:
+#     n=input("Ingrese su nombre")
+    
+# op=0
+# total=0
+# while op!=4:
+#     try:
+#         print("1.- Xbox Series S $250.000")
+#         print("2.- Sony PS5 $ $500.000")
+#         print("3.- LGTV 60 Pulgadas $600.000")
+#         print("4.- Salir")
+#         op=int(input("Seleccione una opcion: "))
+#     except:
+#         print("SOLO SE ACEPTAN NUMEROS ENTEROS")
+#     match op:
+#         case 1:
+#             print("El valor a pagar ", 250000*1.19)
+#             total+=250000*1.19
+#         case 2:
+#             print("El valor a pagar ", 500000*1.19)
+#             total+=500000*1.19
+#         case 3:
+#             print("El valor a pagar ", 600000*1.19)
+#             total+=600000*1.19
+#         case 4:
+#             print("Saliendo")
+#             print(f"El total a pagar es {total}")
+#         case _:
+#             print("Opcion Invalida")
+
+while True:
+    try:
+        notas=int(input("Ingrese la cant de notas: ")) # aqui
+        break
+    except:
+        print("Ingresar solo numeros enteros")
+
+
+# suma=0
+# for i in range(notas):
+#     while True:
+#         try:
+#             n=float(input(f"Ingrese la nota {i+1}: ")) #acá
+#             break
+#         except:
+#             print("Ingresar solo numeros enteros")
+    
+#     suma=suma+n
+#     # suma+=n
+# prom=suma/notas
+# print("El promedio es",round(prom,1) )
+
+# if prom>=4:
+#     print("Alumno aprobado")
+# else:
+#     print("Alumno reprobado")
+
+
