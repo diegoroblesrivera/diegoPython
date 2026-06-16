c999e687e51d1007a56e92891e6fc040abe5b5ac
Diegorr <diegoroblesrivera@gmail.com>
2026-06-16 10:54:08 -0400

    Ejercicopara prueba 4

---
+++ 002/EjerciciosPrueba4/eje1.py
@@ -0,0 +1,19 @@
+## crear un gestor de estacionamiento
+# Un estacionamiento tiene 4 pisos
+# y cada piso tiene 10 espacios
+#  Preguntar cuando entra un vehiculo, que tipo de vheiculo es
+# vehículo ligero 2000
+# vehículo mediano 3000
+# vehículo pesado 3500
+# luego , acomodarlo en algun lugar de algun piso disponible.
+# el menu dsebe tener las sigueintes alternativas
+''' 1.- ingresar vehiculo
+2.- contar ganancias
+3.- contar vehiculos'''
+# usa lista o diccionario segun le acomode mas
+parking={
+    1:[2000, 3500, 2000,2000, 3500, 2000,2000, 3500, 2000],
+    2:[],
+    3:[],
+    4:[]
+}
