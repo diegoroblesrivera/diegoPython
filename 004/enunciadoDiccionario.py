475acb3fcefe6a179bce520fde26dc1d4ceead75
Diegorr <diegoroblesrivera@gmail.com>
2026-06-16 16:48:04 -0400

    Ejercicios y tareas para Eva04

--- 004/enunciadoDiccionario.py
+++ 004/enunciadoDiccionario.py
@@ -5,15 +5,12 @@
 # vehículo ligero 2000
 # vehículo mediano 3000
 # vehículo pesado 3500
-
 # luego , acomodarlo en algun lugar de algun piso disponible.
 # el menu dsebe tener las sigueintes alternativas
 ''' 1.- ingresar vehiculo
 2.- contar ganancias
 3.- contar vehiculos'''
-
 # usa lista o diccionario segun le acomode mas
-
 parking={
     1:[2000, 3500, 2000,2000, 3500, 2000,2000, 3500, 2000],
     2:[],
---
+++ 006/EjerciciosPrueba4/eje2.py
@@ -0,0 +1,17 @@
+## Crear un gestor de pacientes
+
+pacientes=[
+    {"nombre": " Aquiles Baeza", "prevision": "Fonasa", 
+     "temperatura":34.6, "grave": False}
+]
+
+'''crear al gestor de pacientes en un centro medico
+Para poner el nombre se debe validar que no este vacio 
+y ademas tenga mas de 8 caracteres
+Para la prevision de salud solo exiten 3 posibles valores
+Fonasa, Isapre, o Fodesa
+Al ingresar un paciente, se debe poner la temperatura
+Crear una funcion que valide si esta grave o no
+Para que este grave debe tener mas de 39°'''
+
+
